d64649a1f6b96cbedfe05507b11f698cdc133ede
jayyjayy <164137318+jayyjayy@users.noreply.github.com>
2024-05-07 15:22:49 +0800

    Create rjayguitars.php

---
+++ rjayguitars.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>RjayGuitars</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+    <div class="Header" id="2hd">
+        <H1 class="bname">RjayGuitars</H1>
+        <p class="bd">Building high quality and affordable guitars</p>
+    </div>
+    <nav class="navbar">
+        <ul>
+            <li><a href="http://localhost/EcomWeb/acoustic.php">Acoustic</a></li>
+            <li><a href="electric.html">Electric</a></li> 
+            <li><a href="classical.html">Classical</a></li> 
+            <li><a href="bass.html">Bass</a></li> 
+            <li><a href="file:///C:/xampp/htdocs/Ecomm/RjayGuitars.html">Home</a></li>     
+        </ul> 
+    </nav>
+    <div class="info">
+        <aside class="description">
+            <p>The RjayGuitars Solid Acoustic Series is built from solid wood,
+                 allowing it to vibrate freely and produce rich, 
+                dynamic tones that are simply unmatched. 
+                It is equipped with D'Addario strings, 
+                making every strum a premium experience.</p>
+        </aside>
+        <aside class="pict">
+            <img src="Images/guitar2.jpeg">
+        </aside>
+    </div>
+    <div class="footer">
+        <h4>ABOUT RjayGuitars</h4>
+            <p class="footc">We are a maker of world-class and affordable Philippine-made guitars and
+                instruments since 2023. With over three decades 
+                of experience, we have been dedicated to producing
+                high-quality and exceptional-sounding instruments that
+                cater to both beginners and professionals.</p>
+        <h4>CONNECT WITH US</h4>
+        <ul class="cws">
+            <li><img src="Images/fb2.jpg"></li>
+            <li><img src="Images/tt.jpg"></li>
+            <li><img src="Images/yt.jpg"></li>
+            <li><img src="Images/ins.jpg"></li>
+        </ul>    
+    </div>
+</body>
+</html>
